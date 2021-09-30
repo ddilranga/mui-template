4155f1d15d1a8c3ded3a72e4d6cc891092001819
zenhac <40557818+zenhac@users.noreply.github.com>
2021-09-30 17:19:37 +0530

    path alias resolving for tsconfig & vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      components: resolve(__dirname, "./src/components"),
+      views: resolve(__dirname, "./src/views"),
+      layouts: resolve(__dirname, "./src/layouts"),
+      hooks: resolve(__dirname, "./src/hooks"),
+      services: resolve(__dirname, "./src/services"),
+      utils: resolve(__dirname, "./src/utils"),
+      assets: resolve(__dirname, "./src/assets"),
+    }
+  }
 })
