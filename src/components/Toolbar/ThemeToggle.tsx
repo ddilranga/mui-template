5c9d6fe6743a594f9ff34d29ddd5eae21422c1d9
zenhac <40557818+zenhac@users.noreply.github.com>
2021-10-07 19:49:44 +0530

    theme toggle button enhancements

--- src/components/Toolbar/ThemeToggle.tsx
+++ src/components/Toolbar/ThemeToggle.tsx
@@ -1,6 +1,7 @@
-import Brightness4Icon from "@mui/icons-material/Brightness4";
-import Brightness7Icon from "@mui/icons-material/Brightness7";
-import Box from "@mui/material/Box";
+import {
+  DarkMode as MoonIcon,
+  LightMode as SunIcon
+} from "@mui/icons-material";
 import IconButton from "@mui/material/IconButton";
 import { useTheme } from "@mui/material/styles";
 import { ColorModeContext } from "configs";
@@ -11,30 +12,13 @@ const ThemeToggle = () => {
   const colorMode = React.useContext(ColorModeContext);
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        width: "100%",
-        alignItems: "center",
-        justifyContent: "center",
-        bgcolor: "background.default",
-        color: "text.primary",
-        borderRadius: 1,
-        p: 3
-      }}
+    <IconButton
+      sx={{ ml: 1, bgcolor: "background.default", color: "text.primary" }}
+      onClick={colorMode.toggleColorMode}
+      color="inherit"
     >
-      <IconButton
-        sx={{ ml: 1 }}
-        onClick={colorMode.toggleColorMode}
-        color="inherit"
-      >
-        {theme.palette.mode === "dark" ? (
-          <Brightness7Icon />
-        ) : (
-          <Brightness4Icon />
-        )}
-      </IconButton>
-    </Box>
+      {theme.palette.mode === "dark" ? <MoonIcon /> : <SunIcon />}
+    </IconButton>
   );
 };
 
