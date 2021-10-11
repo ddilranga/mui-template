7300f843a6bba77e62cde0f2e3464b1b3af79a91
zenhac <40557818+zenhac@users.noreply.github.com>
2021-10-12 00:56:02 +0530

    changed routes function to a route array

--- src/configs/routes.tsx
+++ src/configs/routes.tsx
@@ -1,36 +1,32 @@
 import { Navigate, RouteObject } from "react-router";
 import Dashboard from "views/dashboard/Dashboard";
 
-const routes = () => {
-	const routesArr: RouteObject[] = [
-		{
-			path: "/",
-			element: <Navigate to="/app/dashboard" />,
-		},
-		{
-			path: "app",
-			children: [
-				{
-					path: "/app",
-					element: <Navigate to="/app/dashboard" />,
-				},
-				{
-					path: "dashboard",
-					element: <Dashboard />,
-				},
-			],
-		},
-		{
-			path: "404",
-			element: <>404</>,
-		},
-		{
-			path: "*",
-			element: <Navigate to="/404" />,
-		},
-	];
-
-	return routesArr;
-};
+const routes: RouteObject[] = [
+	{
+		path: "/",
+		element: <Navigate to="/app/dashboard" />,
+	},
+	{
+		path: "app",
+		children: [
+			{
+				path: "/app",
+				element: <Navigate to="/app/dashboard" />,
+			},
+			{
+				path: "dashboard",
+				element: <Dashboard />,
+			},
+		],
+	},
+	{
+		path: "404",
+		element: <>404</>,
+	},
+	{
+		path: "*",
+		element: <Navigate to="/404" />,
+	},
+];
 
 export default routes;
