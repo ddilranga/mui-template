36ca4a8e31450d002d8df7e159d782a362558254
zenhac <40557818+zenhac@users.noreply.github.com>
2021-10-11 23:46:04 +0530

    sidebar context consumer hook and provider created

---
+++ src/components/Sidebar/Sidebar.context.tsx
@@ -0,0 +1,41 @@
+import { createContext, ReactNode, useContext, useMemo } from "react";
+import { ISidebarProps } from "./Sidebar.types";
+
+const SidebarContext = createContext<{
+	onLinkClick: ISidebarProps["onLinkClick"];
+}>({ onLinkClick: () => {} });
+
+function useSidebarContext() {
+	const context = useContext(SidebarContext);
+
+	if (context === undefined) {
+		throw new Error(
+			"useSidebarContext must be within a SidebarContext Provider!"
+		);
+	}
+
+	return context;
+}
+
+function SidebarContextProvider({
+	children,
+	value: { onLinkClick },
+}: {
+	children: ReactNode;
+	value: { onLinkClick: ISidebarProps["onLinkClick"] };
+}) {
+	const memoizedValue = useMemo(
+		() => ({
+			onLinkClick,
+		}),
+		[onLinkClick]
+	);
+
+	return (
+		<SidebarContext.Provider value={memoizedValue}>
+			{children}
+		</SidebarContext.Provider>
+	);
+}
+
+export { useSidebarContext, SidebarContextProvider };
