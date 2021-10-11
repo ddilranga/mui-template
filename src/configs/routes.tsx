127972be7808aa58288bbc027ee6797f4c33d419
zenhac <40557818+zenhac@users.noreply.github.com>
2021-10-11 23:44:01 +0530

    dashboard dummy component added to routes

--- src/configs/routes.tsx
+++ src/configs/routes.tsx
@@ -1,35 +1,36 @@
 import { Navigate, RouteObject } from "react-router";
+import Dashboard from "views/dashboard/Dashboard";
 
 const routes = () => {
-  const routesArr: RouteObject[] = [
-    {
-      path: "/",
-      element: <Navigate to="/app/dashboard" />
-    },
-    {
-      path: "app",
-      children: [
-        {
-          path: "/app",
-          element: <Navigate to="/app/dashboard" />
-        },
-        {
-          path: "dashboard",
-          element: <>App Dashboard</>
-        }
-      ]
-    },
-    {
-      path: "404",
-      element: <>404</>,
-    },
-    {
-      path: "*",
-      element: <Navigate to="/404" />
-    }
-  ]
+	const routesArr: RouteObject[] = [
+		{
+			path: "/",
+			element: <Navigate to="/app/dashboard" />,
+		},
+		{
+			path: "app",
+			children: [
+				{
+					path: "/app",
+					element: <Navigate to="/app/dashboard" />,
+				},
+				{
+					path: "dashboard",
+					element: <Dashboard />,
+				},
+			],
+		},
+		{
+			path: "404",
+			element: <>404</>,
+		},
+		{
+			path: "*",
+			element: <Navigate to="/404" />,
+		},
+	];
 
-  return routesArr;
-}
+	return routesArr;
+};
 
 export default routes;
