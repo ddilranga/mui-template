dfc5fb11e833dcb945469fc4e1755d2c5e1795ec
zenhac <40557818+zenhac@users.noreply.github.com>
2021-10-20 17:49:35 +0530

    auth layout created

---
+++ src/layouts/AuthLayout.tsx
@@ -0,0 +1,20 @@
+import { Box } from "@mui/system";
+import { Outlet } from "react-router";
+
+const AuthLayout = () => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        flexWrap: "wrap",
+        minHeight: "100vh",
+      }}
+    >
+      <div>Login Layout</div>
+      <Outlet />
+    </Box>
+  );
+};
+
+export default AuthLayout;
