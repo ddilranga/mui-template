00f48bc289e2dfffc616c14ec40c35ce8fad40a7
zenhac <40557818+zenhac@users.noreply.github.com>
2021-10-20 23:24:52 +0530

    removed unwanted layout config file

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,4 +1,3 @@
 export { default as ThemeProvider, ColorModeContext } from "./theme";
-export { default as Layout } from "./layout";
 export { default as routes } from "./routes";
 export { default as navigation } from "./navigation";
