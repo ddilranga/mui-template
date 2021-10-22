69a136162e68d0114dc2d13c48e2fa2c3cf66173
zenhac <40557818+zenhac@users.noreply.github.com>
2021-10-23 00:15:56 +0530

    created a reducer manager for easier code splitting redux modules

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { createReducerManager } from "./reducerManager";
+
+const middlewares: [] = [];
+
+const staticReducers = {};
+
+export const reducerManager = createReducerManager(staticReducers);
 
 export const store = configureStore({
-  reducer: {},
+  reducer: reducerManager.reduce,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(middlewares),
+  devTools: import.meta.env.DEV,
 });
 
 export type AppDispatch = typeof store.dispatch;
