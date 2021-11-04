6b6874bb4a92cf1b6aa28a2efd4fde357488b988
zenhac <40557818+zenhac@users.noreply.github.com>
2021-11-04 22:17:03 +0530

    login fake data handler created

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,24 +1,45 @@
 // src/mocks/handlers.js
+import { nanoid } from "@reduxjs/toolkit";
 import { rest } from "msw";
+import { AuthState } from "views/auth/store/types";
 
 const baseURL = (path: string | URL) => {
   return new URL(`backend/${path}`, "http://localhost:3000").toString();
 };
 
+const token = nanoid();
+
 export const handlers = [
   rest.post(baseURL("login"), (req, res, ctx) => {
     // Persist user's authentication in the session
-    sessionStorage.setItem("is-authenticated", "true");
+    sessionStorage.setItem("token", token);
 
     return res(
-      // Respond with a 200 status code
-      ctx.status(200)
+      ctx.delay(400),
+      ctx.json<AuthState>({
+        user: {
+          first_name: "Test",
+          last_name: "User",
+        },
+        token,
+      })
+    );
+  }),
+
+  rest.get(baseURL("example"), (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json<{ response: { example: string } }>({
+        response: {
+          example: "response from server",
+        },
+      })
     );
   }),
 
   rest.get(baseURL("user"), (req, res, ctx) => {
     // Check if the user is authenticated in this session
-    const isAuthenticated = sessionStorage.getItem("is-authenticated");
+    const isAuthenticated = sessionStorage.getItem("token");
 
     if (!isAuthenticated) {
       // If not authenticated, respond with a 403 error
