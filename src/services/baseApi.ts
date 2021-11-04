e4c9dc35cabd3047dcc932e9aeb351c5be9d0be0
zenhac <40557818+zenhac@users.noreply.github.com>
2021-11-04 22:18:25 +0530

    base api header preparation with auth token

--- src/services/baseApi.ts
+++ src/services/baseApi.ts
@@ -1,6 +1,18 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { RootState } from "store";
 
 export const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: "/backend" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "/backend",
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).auth?.token;
+
+      if (token) {
+        headers.set("authorization", `Bearer ${token}`);
+      }
+
+      return headers;
+    },
+  }),
   endpoints: () => ({}),
 });
