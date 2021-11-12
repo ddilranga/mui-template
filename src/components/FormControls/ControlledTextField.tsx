279232a3fd85c6488b39512c566b2c0815bc39a9
zenhac <40557818+zenhac@users.noreply.github.com>
2021-11-13 00:05:10 +0530

    controlled text field componet created

---
+++ src/components/FormControls/ControlledTextField.tsx
@@ -0,0 +1,26 @@
+import { TextField, TextFieldProps } from "@mui/material";
+import { Control, Controller } from "react-hook-form";
+
+type ControlledTextFieldProps = {
+  name: any;
+  control: Control<any>;
+} & TextFieldProps;
+
+const ControlledTextField = (props: ControlledTextFieldProps) => {
+  return (
+    <Controller
+      name={props.name}
+      control={props.control}
+      render={({ field, fieldState: { error } }) => (
+        <TextField
+          {...field}
+          {...props}
+          error={Boolean(error)}
+          helperText={error && error.message}
+        />
+      )}
+    />
+  );
+};
+
+export default ControlledTextField;
