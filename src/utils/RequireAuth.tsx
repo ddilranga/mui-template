a188fdf1ee2cc0ae53056653bcd1a929f5c7cebc
zenhac <40557818+zenhac@users.noreply.github.com>
2021-12-07 19:52:33 +0530

    require auth utility variable changes

--- src/utils/RequireAuth.tsx
+++ src/utils/RequireAuth.tsx
@@ -2,8 +2,8 @@ import { useAuth } from "hooks";
 import { Navigate, useLocation } from "react-router-dom";
 
 export default function RequireAuth({ children }: { children: JSX.Element }) {
-  let auth = useAuth();
-  let location = useLocation();
+  const auth = useAuth();
+  const location = useLocation();
 
   if (!auth.user) {
     return <Navigate to="/auth/login" state={{ from: location }} />;
