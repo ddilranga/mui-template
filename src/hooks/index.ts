b87125719634380a7de2bd4aab67d7ebdaa94748
zenhac <40557818+zenhac@users.noreply.github.com>
2021-12-07 18:20:52 +0530

    use event, local, session hooks created

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,5 @@
 export * from "./store";
 export { default as useAuth } from "./useAuth";
+export { default as useLocalStorage } from "./useLocalStorage";
+export { default as useSessionStorage } from "./useSessionStorage";
+export { default as useEventListener } from "./useEventListener";
