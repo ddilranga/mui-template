e922865da58220c0a9f32999467545046717c75d
ddilranga <40557818+ddilranga@users.noreply.github.com>
2021-12-24 00:45:28 +0530

    removed unused hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,3 @@
 export * from "./store";
 export { default as useAuth } from "./useAuth";
-export { default as useLocalStorage } from "./useLocalStorage";
-export { default as useSessionStorage } from "./useSessionStorage";
 export { default as useEventListener } from "./useEventListener";
