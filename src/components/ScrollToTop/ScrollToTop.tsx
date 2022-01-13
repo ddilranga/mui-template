75ad5a1c45c3fbfdee5fad51cb2ff757bf1418d9
ddilranga <40557818+ddilranga@users.noreply.github.com>
2022-01-13 22:42:08 +0530

    scroll to top component created

---
+++ src/components/ScrollToTop/ScrollToTop.tsx
@@ -0,0 +1,38 @@
+import { Box, useScrollTrigger, Zoom } from "@mui/material";
+import { ReactElement } from "react";
+
+type ScrollToTopProps = { children: ReactElement; target: string };
+
+const ScrollToTop = ({ children, target }: ScrollToTopProps) => {
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 100,
+  });
+
+  const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    const anchor = (
+      (event.target as HTMLDivElement).ownerDocument || document
+    ).querySelector(target);
+
+    if (anchor) {
+      anchor.scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+      });
+    }
+  };
+
+  return (
+    <Zoom in={trigger}>
+      <Box
+        onClick={handleClick}
+        role="presentation"
+        sx={{ position: "fixed", bottom: 16, right: 16 }}
+      >
+        {children}
+      </Box>
+    </Zoom>
+  );
+};
+
+export default ScrollToTop;
